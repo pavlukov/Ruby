9475e76ce7195d6d10eeba5c060a51d9515287cb
Kilias <sergeybmzh@gmail.com>
2018-03-24 00:12:35 +0300

    Callback for Teacher added

--- Rails/blog/app/models/teacher.rb
+++ Rails/blog/app/models/teacher.rb
@@ -1,4 +1,11 @@
 class Teacher < ApplicationRecord
   has_many :lectures
   has_many :students, through: :lectures
+  validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/,
+    message: "Name must consist of letters only!" }
+  after_save :save_message
+
+  def save_message
+    puts "succesfully saved."
+  end
 end
